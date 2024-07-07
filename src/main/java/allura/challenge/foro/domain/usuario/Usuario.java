a6ffdfb2847b09ed6482443b9966623d9ea0c4db
Juan Carlos Del Rio <jcdre@hotmail.com>
2024-07-06 20:22:57 -0500

    Tabla para usuarios

---
+++ src/main/java/allura/challenge/foro/domain/usuario/Usuario.java
@@ -0,0 +1,23 @@
+package allura.challenge.foro.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String nombre;
+  private String email;
+  private String usuario;
+  private String clave;
+}
