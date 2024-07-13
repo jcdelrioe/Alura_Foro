4a6c95eaf5433705de833280debb0fed4153ef83
Juan Carlos Del Rio <jcdre@hotmail.com>
2024-07-13 05:47:48 -0500

    Session STATELESS

--- src/main/java/allura/challenge/foro/domain/usuario/UsuarioRepository.java
+++ src/main/java/allura/challenge/foro/domain/usuario/UsuarioRepository.java
@@ -1,7 +1,9 @@
 package allura.challenge.foro.domain.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+  UserDetails findByUsuario(String username);
 }
---
+++ src/main/java/allura/challenge/foro/infra/security/AutenticationService.java
@@ -0,0 +1,20 @@
+package allura.challenge.foro.infra.security;
+
+import allura.challenge.foro.domain.usuario.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticationService implements UserDetailsService {
+
+  @Autowired
+  private UsuarioRepository usuarioRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return usuarioRepository.findByUsuario(username);
+  }
+}
